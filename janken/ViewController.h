6ccc8d6fd9f4c75378a774009972ba8ce4bcad3c
Kitao Akiko <akiko@Kitao-no-MacBook.local>
2013-02-14 01:15:27 +0900

    1st install to iPhone

--- janken/ViewController.h
+++ janken/ViewController.h
@@ -8,6 +8,29 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UIButton *btn_gu;
+    IBOutlet UIButton *btn_tyoki;
+    IBOutlet UIButton *btn_pa;
+    IBOutlet UIButton *btn_again;
+    IBOutlet UILabel  *lbl_mes;
+    IBOutlet UILabel  *lbl_kekka;
+    IBOutlet UILabel  *lbl_aite;
+    IBOutlet UIImageView *aite_img;
+}
+
+@property(nonatomic,retain) UIButton *btn_gu;
+@property(nonatomic,retain) UIButton *btn_tyoki;
+@property(nonatomic,retain) UIButton *btn_pa;
+@property(nonatomic,retain) UIButton *btn_again;
+@property(nonatomic,retain) UILabel  *lbl_mes;
+@property(nonatomic,retain) UILabel  *lbl_kekka;
+@property(nonatomic,retain) UILabel  *lbl_aite;
+@property(nonatomic,retain) UIImageView *aite_img;
+
+- (IBAction)btn_gu_down:(id)sender;
+- (IBAction)btn_tyoki_down:(id)sender;
+- (IBAction)btn_pa_down:(id)sender;
+- (IBAction)btn_again_down:(id)sender;
 
 @end
--- janken/ViewController.m
+++ janken/ViewController.m
@@ -14,10 +14,136 @@
 
 @implementation ViewController
 
+@synthesize btn_gu, btn_tyoki, btn_pa, btn_again;
+@synthesize lbl_mes,lbl_kekka,lbl_aite, aite_img;
+
+UIImage *gu_img;
+UIImage *ch_img;
+UIImage *pa_img;
+
+- (IBAction)btn_gu_down:(id)sender {
+    self.lbl_mes.text = @"じゃんけん・・・ぽん";
+    self.btn_pa.hidden = YES;
+    self.btn_tyoki.hidden = YES;
+    self.lbl_aite.hidden = NO;
+    self.aite_img.hidden = NO;
+    self.btn_again.hidden = NO;
+    
+    srand(time(nil));
+    NSInteger kekka;
+    kekka = rand()%3;
+    if (kekka == 0) {
+       self.aite_img.image = gu_img;
+    } else if (kekka == 1) {
+       self.aite_img.image = ch_img;
+    } else {
+       self.aite_img.image = pa_img;
+    }
+    
+    if (kekka == 0) {
+        self.lbl_kekka.text = @"あいこで・・・";
+        self.btn_pa.hidden = NO;
+        self.btn_tyoki.hidden = NO;
+        self.btn_again.hidden = YES;
+    } else if (kekka == 1) {
+        self.lbl_kekka.text = @"あなたのかち";
+        self.btn_gu.enabled = NO;
+    } else {
+        self.lbl_kekka.text = @"あなたのまけ";
+        self.btn_gu.enabled = NO;
+    }
+}
+
+- (IBAction)btn_tyoki_down:(id)sender {
+    self.lbl_mes.text = @"じゃんけん・・・ぽん";
+    self.btn_pa.hidden = YES;
+    self.btn_gu.hidden = YES;
+    self.lbl_aite.hidden = NO;
+    self.aite_img.hidden = NO;
+    self.btn_again.hidden = NO;
+    
+    srand(time(nil));
+    NSInteger kekka;
+    kekka = rand()%3;
+    if (kekka == 0) {
+        self.aite_img.image = gu_img;
+    } else if (kekka == 1) {
+        self.aite_img.image = ch_img;
+    } else {
+        self.aite_img.image = pa_img;
+    }
+    
+    if (kekka == 1) {
+        self.lbl_kekka.text = @"あいこで・・・";
+        self.btn_pa.hidden = NO;
+        self.btn_gu.hidden = NO;
+        self.btn_again.hidden = YES;
+    } else if (kekka == 2) {
+        self.lbl_kekka.text = @"あなたのかち";
+        self.btn_tyoki.enabled = NO;
+    } else {
+        self.lbl_kekka.text = @"あなたのまけ";
+        self.btn_tyoki.enabled = NO;
+    }
+}
+
+- (IBAction)btn_pa_down:(id)sender {
+    self.lbl_mes.text = @"じゃんけん・・・ぽん";
+    self.btn_gu.hidden = YES;
+    self.btn_tyoki.hidden = YES;
+    self.lbl_aite.hidden = NO;
+    self.aite_img.hidden = NO;
+    self.btn_again.hidden = NO;
+       
+    srand(time(nil));
+    NSInteger kekka;
+    kekka = rand()%3;
+    if (kekka == 0) {
+        self.aite_img.image = gu_img;
+    } else if (kekka == 1) {
+        self.aite_img.image = ch_img;
+    } else {
+        self.aite_img.image = pa_img;
+    }
+    
+    if (kekka == 2) {
+        self.lbl_kekka.text = @"あいこで・・・";
+        self.btn_gu.hidden = NO;
+        self.btn_tyoki.hidden = NO;
+        self.btn_again.hidden = YES;
+    } else if (kekka == 1) {
+        self.lbl_kekka.text = @"あなたのかち";
+        self.btn_pa.enabled = NO;
+    } else {
+        self.lbl_kekka.text = @"あなたのまけ";
+        self.btn_pa.enabled = NO;
+    }
+}
+
+- (IBAction)btn_again_down:(id)sender {
+    self.btn_gu.hidden = NO;
+    self.btn_gu.enabled = YES;
+    self.btn_tyoki.hidden = NO;
+    self.btn_tyoki.enabled = YES;
+    self.btn_pa.hidden = NO;
+    self.btn_pa.enabled = YES;
+    self.btn_again.hidden = YES;
+    self.lbl_aite.hidden = YES;
+        self.aite_img.hidden = YES;
+    self.lbl_mes.text = @"じゃんけん・・・";
+    self.lbl_kekka.text = @"";
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.btn_again.hidden = YES;
+    self.lbl_aite.hidden = YES;
+    self.lbl_kekka.text = @"";
+ 
+    gu_img = [UIImage imageNamed:@"gu.png"];
+    ch_img = [UIImage imageNamed:@"ch.png"];
+    pa_img = [UIImage imageNamed:@"pa.png"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +151,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
